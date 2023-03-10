773aaf4115e6d69a218c7ea7159493f1ea07eb22
Oscar Castro <oscar.a.castro818@gmail.com>
2023-03-10 16:33:29 -0600

    feat: created footer component and styled

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+
+    return (
+        <footer className="footer p-10 bg-gray-700 text-primary-content footer-center">
+            Footer text goes here
+        </footer>
+    )
+}
+
+export default Footer;
