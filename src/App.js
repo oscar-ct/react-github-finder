3ed13aa687a6494c2f4a8cf6fd34bcd4feaa7462
Oscar Castro <oscar.a.castro818@gmail.com>
2023-03-10 16:26:11 -0600

    feat: created navbar component and styled

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
 
 function App() {
   return (
-    <>
-        <div className="bg-purple-500">
-            <h1 className="text-4xl">Hello World</h1>
-            <button className="btn">Click</button>
-        </div>
-    </>
+      <Router>
+          <div className="flex flex-col justify-between h-screen">
+              <Navbar/>
+              <main>Content</main>
+          </div>
+      </Router>
   );
 }
 
