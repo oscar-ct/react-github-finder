14a3841b9e55da5c38e2d08bfd86e62ec3e531cb
Oscar Castro <oscar.a.castro818@gmail.com>
2023-03-11 13:04:00 -0600

    import and set Alert component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import UserResults from "../components/users/UserResults";
 import UserSearch from "../components/users/UserSearch";
+import Alert from "../components/layout/Alert";
 
 
 const Home = () => {
     return (
         <>
+            <Alert/>
             <UserSearch/>
             <UserResults/>
         </>
