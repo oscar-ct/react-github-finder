45114f0600fd6427bf0c8b4e69a5d3d12af60ae5
Oscar Castro <oscar.a.castro818@gmail.com>
2023-03-11 00:49:56 -0600

    feat: used state and context to handle click events

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,13 +1,50 @@
 import React from 'react';
+import {useState, useContext} from "react";
+import GithubContext from "../../context/github/GithubContext";
 
 const UserSearch = () => {
+
+    const [text, setText] = useState("");
+
+    // This context data is for the clear btn
+    const { users } = useContext(GithubContext);
+
+
+    const handleChange = (e) => {
+        setText(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (text === "") {
+            window.alert("Please enter something");
+        } else {
+            setText("");
+        }
+    }
+
+    const usersExist = () => {
+        if (users.length === 0)
+        return (
+            <div>
+                <button className="btn btn-ghost btn-lg">
+                    Clear
+                </button>
+            </div>
+        )
+    }
+
     return (
         <div className="grid grid-cols-1 xl:grids-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
             <div>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="form-control">
                         <div className="relative">
-                            <input className="w-full pr-40 bg-gray-500 input input-lg text-white" placeholder="Search"
+                            <input
+                                className="w-full pr-40 bg-gray-500 input input-lg text-white"
+                                placeholder="Search"
+                                value={text}
+                                onChange={handleChange}
                             />
                             <button className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg">
                                 Go
@@ -17,9 +54,8 @@ const UserSearch = () => {
 
                 </form>
             </div>
-            <div>
-                <button className="btn btn-ghost btn-lg">Clear</button>
-            </div>
+            {/* CLEAR button */}
+            {usersExist()}
         </div>
     );
 };
